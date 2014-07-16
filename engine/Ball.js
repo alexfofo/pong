3ba3bd0b3277502f954e181ae91cd4235bfda7b5
Guillaume Casalis <gcasalis@gmail.com>
2014-07-16 17:45:51 +0200

    Add a size to Ball

--- engine/Ball.js
+++ engine/Ball.js
@@ -7,6 +7,7 @@ var util = require('util'),
 function Ball(options) {
     options = options || {};
 
+    options.size = Ball.SIZE.clone();
     options.direction = Ball.DIRECTION_START.clone();
     options.speed = Ball.SPEED_START;
 
@@ -18,5 +19,7 @@ util.inherits(Ball, Entity);
 Ball.DIRECTION_START = new Vector(1, 0);
 
 Ball.SPEED_START = 0.2; // distance unit / second
+
+Ball.SIZE = new Vector(0.1, 0.1);
 
 module.exports = Ball;
